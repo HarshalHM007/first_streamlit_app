691180a6b62dd1bd4e54165eee6b2ae483ec924e
Harshal More <120248685+HarshalHM007@users.noreply.github.com>
2022-12-10 15:31:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,10 @@
 import streamlit
 
 streamlit.title('My Moms New Healthy Diner')
+streamlit.header('Breakfast Favourites')
+streamlit.text('🥣Omega 3 & Blueberry oatmeal')
+streamlit.text('🥗Kale, Spinch & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
